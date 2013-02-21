4746279f7925e3f3305b87f4f7bf47a4fa3bb788
Bram van der Veen <96aa48@gmail.com>
2013-02-21 23:14:08 +0100

    Removed Injection function. Changed login into getLogin() Added MineAuth for Auth.

--- functions.js
+++ functions.js
@@ -27,6 +27,8 @@ function setupEvents() {
 	
 	$("#play a").click(function(e){
 		
+		//$('#iframe').attr("src","http://dev.hexedlime.tk/minecraft/minecraft.html");
+		
 		$('#login').slideToggle("slow")
 		
 		$('[name="savepassword"]').click(function(e){
@@ -37,12 +39,12 @@ function setupEvents() {
 					alert("Storing your password won't be completely safe, continue on own risks.");
 				}
 			
-		});		
+		});
 	});
 	
 	$("[name='login']").click(function(e){
 			
-		login();
+		getLogin();
 		
 	});
 	
@@ -57,10 +59,7 @@ function setupEvents() {
 		fullscreen();
 	
 	});
-		
-	$("#iframe").load(function(e){
-		injection(e);
-	});
+	
 }
 
 function getData() {
@@ -101,7 +100,7 @@ function getData() {
 	
 }
 
-function login() {
+function getLogin() {
 		
 		var saveusername = $("[name='saveusername']:checked").val();
 		var username = $("[name='username']").val();
@@ -123,9 +122,37 @@ function login() {
 				chrome.storage.sync.set({'password':password});
 				
 			}
-		
-		$("#iframe").attr("src","http://minecraft.net/play");
 	
+	if (authLogin(username,password) == 1) {
+		
+	}
+	else {
+		
+	}
+	
+}
+
+function authLogin(u,p) {
+	$.get('http://login.minecraft.net/?user=' + u + '&password=' + p +'&version=69',function(e){
+		var auth = e.split(':');
+		
+		if (auth[0] == "Account migrated, use e-mail as username.") {
+			alert(auth[0]);
+		}
+		else if (auth[0] == "Bad login") {
+			alert("Bad login: please check your Minecraft login information again.");
+		}
+		else {
+			buildMinecraft(auth);
+		}
+		console.log(auth);
+	});
+}
+
+function buildMinecraft(e) {
+	var link = "http://dev.hexedlime.tk/mc/?u=" + e[2] + "&v=" + e[0] + "&s=" + e[3] + "&d=" + e[4];
+	console.log(link);
+	$("#iframe").attr("src",link);
 }
 
 function checkOnline() {
@@ -153,7 +180,8 @@ function download() {
 
 function offline() {
 	
-	$("ul#login").html("<h4>You're offline.</h4><br><h5>Wanna play offline?</h5>");
+	$("ul#login").html("<h4>You're offline.</h4><br><h5>Wanna play offline?</h5><br><h5>This will be available in the future.</h5>");
+	
 	
 }
 
@@ -163,6 +191,8 @@ function removeLogin() {
 		chrome.storage.sync.remove('password');
 		$("[name='username']").val("Username");
 		$("[name='password']").val("Password");
+		
+		alert("Removed all of your info stored by this application");
 	
 }
 
@@ -196,16 +226,4 @@ function fullscreen() {
 					});
 		});
 	}
-}
-
-function injection(x) {
-	var src = x.target.src;
-	
-	if (src == "http://minecraft.net/play") {
-		
-	}
-	else if (src == "http://minecraft.net/login") {
-		alert("Login")
-	}
-	
 }
